18a621e4e451facf0b15836aecab8ea64defef2d
Naveen M K <naveen@syrusdark.website>
2020-12-24 22:26:52 +0530

    Update build_pango.sh

--- packing/build_pango.sh
+++ packing/build_pango.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # build and install pango
-
+set -e
 PANGO_VERSION=1.48.0
 
 cd $TMP
@@ -9,8 +9,8 @@ cd pango
 echo "Downloading Pango"
 
 python -m pip install requests
-python -c "import requests;a=requests.get('http://download.gnome.org/sources/pango/${PANGO_VERSION%.*}/pango-${PANGO_VERSION}.tar.xz');b=open('pango-${PANGO_VERSION}.tar.xz','wb');b.write(a.content)"
-python -m pip install -y requests
+python -c "import requests;a=requests.get('http://download.gnome.org/sources/pango/${PANGO_VERSION%.*}/pango-${PANGO_VERSION}.tar.xz');b=open('pango-${PANGO_VERSION}.tar.xz','wb');b.write(a.content);print('done'"
+python -m pip uninstall -y requests
 tar -xf pango-${PANGO_VERSION}.tar.xz
 
 mv pango-${PANGO_VERSION} pango
@@ -19,7 +19,7 @@ echo "Installing Meson and Ninja"
 pip3 install -U meson==0.55.3 ninja
 
 echo "Buildling and Installing Pango"
-yum-builddep -y pango-devel
+# yum-builddep -y pango-devel
 
 meson setup --prefix=/usr --buildtype=release -Dintrospection=disabled pango_builddir pango
 meson compile -C pango_builddir
