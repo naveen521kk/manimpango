72b94c491f26801232a7a349d267987acf3923bc
Syrus Dark <naveen@syrusdark.website>
2020-12-25 00:28:42 +0530

    Build cairo and harfbuzz also

--- packing/build_pango.sh
+++ packing/build_pango.sh
@@ -4,6 +4,8 @@ set -e
 
 PANGO_VERSION=1.48.0
 GLIB_VERSION=2.67.1
+CAIRO_VERSION=1.17.4
+HARFBUZZ_VERSION=2.7.3
 
 FILE_PATH="`dirname \"$0\"`"
 FILE_PATH="`( cd \"$FILE_PATH\" && pwd )`"
@@ -19,6 +21,8 @@ echo "Downloading Pango"
 python -m pip install requests
 python $FILE_PATH/packing/download_and_extract.py "http://download.gnome.org/sources/pango/${PANGO_VERSION%.*}/pango-${PANGO_VERSION}.tar.xz" pango
 python $FILE_PATH/packing/download_and_extract.py "http://download.gnome.org/sources/glib/${GLIB_VERSION%.*}/glib-${GLIB_VERSION}.tar.xz" glib
+python $FILE_PATH/packing/download_and_extract.py "https://www.cairographics.org/snapshots/cairo-${CAIRO_VERSION}.tar.xz" cairo
+python $FILE_PATH/packing/download_and_extract.py "https://github.com/harfbuzz/harfbuzz/releases/download/${HARFBUZZ_VERSION}/harfbuzz-${HARFBUZZ_VERSION}.tar.xz" harfbuzz
 python -m pip uninstall -y requests
 
 echo "Installing Meson and Ninja"
@@ -28,6 +32,16 @@ echo "Building and Install Glib"
 meson setup --prefix=/usr --buildtype=release glib_builddir glib
 meson compile -C glib_builddir
 meson install -C glib_builddir
+
+echo "Building and Install Cairo"
+meson setup --prefix=/usr --buildtype=release -Dfontconfig=enabled -Dfreetype=enabled -Dglib=enabled -Dzlib=enabled -Dtee=enabled cairo_builddir cairo
+meson compile -C cairo_builddir
+meson install -C cairo_builddir
+
+echo "Building and Install Harfbuzz"
+meson setup --prefix=/usr --buildtype=release -Dfontconfig=enabled -Dfreetype=enabled harfbuzz_builddir harfbuzz
+meson compile -C harfbuzz_builddir
+meson install -C harfbuzz_builddir
 
 echo "Buildling and Installing Pango"
 meson setup --prefix=/usr --buildtype=release -Dintrospection=disabled pango_builddir pango
