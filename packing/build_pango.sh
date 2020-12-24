0420b528fdc1f807ea9205725d7dd31feaa7725a
Naveen M K <naveen@syrusdark.website>
2020-12-24 22:06:46 +0530

    Update build_pango.sh

--- packing/build_pango.sh
+++ packing/build_pango.sh
@@ -7,7 +7,7 @@ cd $TMP
 mkdir pango
 cd pango
 echo "Downloading Pango"
-yum install wget
+
 wget -O "pango-${PANGO_VERSION}.tar.xz" "https://download.gnome.org/sources/pango/${PANGO_VERSION%.*}/pango-${PANGO_VERSION}.tar.xz"
 tar -xf pango-${PANGO_VERSION}.tar.xz
 
@@ -17,7 +17,7 @@ echo "Installing Meson and Ninja"
 pip3 install -U meson==0.55.3 ninja
 
 echo "Buildling and Installing Pango"
-yum-builddep pango-devel
+yum-builddep -y pango-devel
 
 meson setup --prefix=/usr --buildtype=release -Dintrospection=disabled pango_builddir pango
 meson compile -C pango_builddir
