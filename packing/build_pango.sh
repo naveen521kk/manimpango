40a7ee855219f8b7beb46c8479da5f8023b6883f
Naveen M K <naveen@syrusdark.website>
2020-12-25 14:59:28 +0530

    Update build_pango.sh

--- packing/build_pango.sh
+++ packing/build_pango.sh
@@ -4,7 +4,6 @@ set -e
 
 PANGO_VERSION=1.42.4
 GLIB_VERSION=2.26.1
-CAIRO_VERSION=1.17.4
 HARFBUZZ_VERSION=2.7.3
 
 FILE_PATH="`dirname \"$0\"`"
@@ -21,7 +20,7 @@ echo "Downloading Pango"
 python -m pip install requests
 python $FILE_PATH/packing/download_and_extract.py "http://download.gnome.org/sources/pango/${PANGO_VERSION%.*}/pango-${PANGO_VERSION}.tar.xz" pango
 python $FILE_PATH/packing/download_and_extract.py "http://download.gnome.org/sources/glib/${GLIB_VERSION%.*}/glib-${GLIB_VERSION}.tar.xz" glib
-python $FILE_PATH/packing/download_and_extract.py "https://gitlab.freedesktop.org/cairo/cairo/-/archive/${CAIRO_VERSION}/cairo-${CAIRO_VERSION}.tar.gz" cairo
+# python $FILE_PATH/packing/download_and_extract.py "https://gitlab.freedesktop.org/cairo/cairo/-/archive/${CAIRO_VERSION}/cairo-${CAIRO_VERSION}.tar.gz" cairo
 python $FILE_PATH/packing/download_and_extract.py "https://github.com/harfbuzz/harfbuzz/releases/download/${HARFBUZZ_VERSION}/harfbuzz-${HARFBUZZ_VERSION}.tar.xz" harfbuzz
 python -m pip uninstall -y requests
 
@@ -33,10 +32,10 @@ meson setup --prefix=/usr --buildtype=release glib_builddir glib
 meson compile -C glib_builddir
 meson install -C glib_builddir
 
-echo "Building and Install Cairo"
-meson setup --prefix=/usr --buildtype=release -Dfontconfig=enabled -Dfreetype=enabled -Dglib=enabled -Dzlib=enabled -Dtee=enabled cairo_builddir cairo
-meson compile -C cairo_builddir
-meson install -C cairo_builddir
+# echo "Building and Install Cairo"
+# meson setup --prefix=/usr --buildtype=release -Dfontconfig=enabled -Dfreetype=enabled -Dglib=enabled -Dzlib=enabled -Dtee=enabled cairo_builddir cairo
+# meson compile -C cairo_builddir
+# meson install -C cairo_builddir
 
 echo "Building and Install Harfbuzz"
 meson setup --prefix=/usr --buildtype=release -Dfontconfig=enabled -Dfreetype=enabled harfbuzz_builddir harfbuzz
