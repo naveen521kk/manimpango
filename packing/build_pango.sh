14a247c01d73cbd2498a8f86568becf500fe526f
Naveen M K <naveen@syrusdark.website>
2020-12-25 15:46:34 +0530

    Pixman

--- packing/build_pango.sh
+++ packing/build_pango.sh
@@ -6,6 +6,7 @@ PANGO_VERSION=1.42.4
 GLIB_VERSION=2.67.1
 FRIBIDI_VERSION=1.0.10
 CAIRO_VERSION=1.17.4
+PIXMAN_VERSION=0.40.0
 
 FILE_PATH="`dirname \"$0\"`"
 FILE_PATH="`( cd \"$FILE_PATH\" && pwd )`"
@@ -23,6 +24,7 @@ python $FILE_PATH/packing/download_and_extract.py "http://download.gnome.org/sou
 python $FILE_PATH/packing/download_and_extract.py "http://download.gnome.org/sources/glib/${GLIB_VERSION%.*}/glib-${GLIB_VERSION}.tar.xz" glib
 python $FILE_PATH/packing/download_and_extract.py "https://github.com/fribidi/fribidi/releases/download/v${FRIBIDI_VERSION}/fribidi-${FRIBIDI_VERSION}.tar.xz" fribidi
 python $FILE_PATH/packing/download_and_extract.py "https://cairographics.org/snapshots/cairo-${CAIRO_VERSION}.tar.xz" cairo
+python $FILE_PATH/packing/download_and_extract.py "https://cairographics.org/releases/pixman-${PIXMAN_VERSION}.tar.xz" pixman
 python -m pip uninstall -y requests
 
 echo "Installing Meson and Ninja"
@@ -37,6 +39,13 @@ echo "Building and Install Fribidi"
 meson setup --prefix=/usr --buildtype=release fribidi_builddir fribidi
 meson compile -C fribidi_builddir
 meson install -C fribidi_builddir
+
+echo "Building and Installing Pixman"
+cd pixman
+./configure
+make
+make install
+cd ..
 
 echo "Building and Installing Cairo"
 cd cairo
