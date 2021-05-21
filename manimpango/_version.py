9538b8b6f5916717ca506f0e08c17fb722b0e02b
Naveen M K <naveen@syrusdark.website>
2021-05-22 02:01:47 +0530

    Start using Meson build System though have setup.py until I test things

--- manimpango/_version.py
+++ manimpango/_version.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
-__version__ = "1.0.0a1"
+__version__ = "1.0.0a0"
+__title__ = "ManimPango"
+__description__ = "Bindings for Pango for using with Manim."
+__url__ = "https://www.manim.community"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Naveen M K"
+__email__ = "naveen@syrusdark.website"
+
+__license__ = "GPLv3"
+__copyright__ = "Copyright (c) 2020-2021 Naveen M K"
